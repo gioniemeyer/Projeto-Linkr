cae6f8661915b3fc07560e80228bbd0c3b31c71c
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-05-25 18:10:09 -0300

    login com localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
 import {Switch, Route, BrowserRouter} from 'react-router-dom'
 import SignInPage from './SignIn/SignInPage';
 import HomePage from './Home/HomePage'
+import { useState } from 'react';
+import UserContext from '../contexts/UserContext';
 export default function App() {
+
+  const [user, setUser] = useState('');
 
   return (
     <BrowserRouter>
-      <Switch>
-      <Route path='/' exact>
-          <HomePage/>
-        </Route>
-        <Route path='/sign-up' exact>
-          <SignInPage />
-        </Route>
-      </Switch>
+      <UserContext.Provider value={{user, setUser}}>
+        <Switch>
+        <Route path='/' exact>
+            <HomePage/>
+          </Route>
+          <Route path='/sign-up' exact>
+            <SignInPage />
+          </Route>
+        </Switch>
+      </UserContext.Provider>
     </BrowserRouter>
-    
   );
 }
 
