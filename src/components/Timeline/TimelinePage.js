db8f7736105446158b0378305996ef9d8a212e88
Iuri Magnago <iurimp91@gmail.com>
2021-05-26 14:59:20 -0300

    Implementado layout mobile na página de timeline

--- src/components/Timeline/TimelinePage.js
+++ src/components/Timeline/TimelinePage.js
@@ -9,13 +9,61 @@ import UserContext from "../../contexts/UserContext";
 
 export default function Timeline() {
     const [TimelinePosts, setTimelinePosts] = useState([]);
-    const [enableLoading, setEnableLoading] = useState(true);
-    const [HashtagList, setHashtagList] = useState([]);
+    //     {
+    //         "id": 2,
+    //         "text": "Never Gonna Give You Up #rickroll",
+    //         "link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    //         "linkTitle": "Rick Astley - Never Gonna Give You Up (Video)",
+    //         "linkDescription": "Rick Astley's official music video for “Never Gonna Give You Up” Listen to Rick Astley: https://RickAstley.lnk.to/_listenYDSubscribe to the official Rick Ast...",
+    //         "linkImage": "https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
+    //         "user": {
+    //             "id": 1,
+    //             "username": "teste",
+    //             "avatar": "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/1/avatar"
+    //         },
+    //         "likes": [
+    //             {
+    //                 "id": 1,
+    //                 "userId": 1,
+    //                 "postId": 2,
+    //                 "createdAt": "2021-05-24T18:55:37.544Z",
+    //                 "updatedAt": "2021-05-24T18:55:37.544Z",
+    //                 "user.id": 1,
+    //                 "user.username": "teste"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         "id": 2,
+    //         "text": "Never Gonna Give You Up #rickroll",
+    //         "link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    //         "linkTitle": "Rick Astley - Never Gonna Give You Up (Video)",
+    //         "linkDescription": "Rick Astley's official music video for “Never Gonna Give You Up” Listen to Rick Astley: https://RickAstley.lnk.to/_listenYDSubscribe to the official Rick Ast...",
+    //         "linkImage": "https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg",
+    //         "user": {
+    //             "id": 1,
+    //             "username": "teste",
+    //             "avatar": "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/1/avatar"
+    //         },
+    //         "likes": [
+    //             {
+    //                 "id": 1,
+    //                 "userId": 1,
+    //                 "postId": 2,
+    //                 "createdAt": "2021-05-24T18:55:37.544Z",
+    //                 "updatedAt": "2021-05-24T18:55:37.544Z",
+    //                 "user.id": 1,
+    //                 "user.username": "teste"
+    //             }
+    //         ]
+    //     }
+    // ]);
+    const [enableLoading, setEnableLoading] = useState(false);
     const { user } = useContext(UserContext);
-    const pessoa = JSON.parse(localStorage.getItem("user"));
+    const localUser = JSON.parse(localStorage.getItem("user"));
 
     useEffect(() => {
-        const config = { headers: { Authorization: `Bearer ${user.token || pessoa.token}` } };
+        const config = { headers: { Authorization: `Bearer ${user.token || localUser.token}` } };
         const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts", config);
 
         request.then(response => {
@@ -26,35 +74,27 @@ export default function Timeline() {
         request.catch(error => {
             alert("Houve uma falha ao obter os posts, por favor, atualize a página.");
             setEnableLoading(false);
-            return(
-                <div className="no-post">Nenhum post encontrado :(</div>
-            );
         });
     }, []);
 
-    // tratar a questão do loading e daquele texto juntos
-    // fazer layout mobile
-
     return(
-        <>
-            <TimelineBody>
-                <TimelineContainer>
-                    <TimelinePostsContainer>
-                        <Title>timeline</Title>
-                        {/* <NewPost /> */}
-                        {
-                            TimelinePosts.length === 0 
-                            ? <div className="no-post">Nenhum post encontrado :(</div> 
-                            : TimelinePosts.map((post, i) => <Post post={post} key={i} />)
-                        }
-                        {enableLoading && <Loading />}
-                    </TimelinePostsContainer>
-                    <div className="trending">
-                        <Trending />
-                    </div>
-                </TimelineContainer>
-            </TimelineBody>
-        </>
+        <TimelineBody>
+            <TimelineContainer>
+                <TimelinePostsContainer>
+                    <Title>timeline</Title>
+                    {/* <NewPost /> */}
+                    {
+                        TimelinePosts.length === 0 && !enableLoading
+                        ? <div className="no-post">Nenhum post encontrado :(</div> 
+                        : TimelinePosts.map((post, i) => <Post post={post} key={i} />)
+                    }
+                    {enableLoading && <Loading />}
+                </TimelinePostsContainer>
+                <div className="trending">
+                    <Trending />
+                </div>
+            </TimelineContainer>
+        </TimelineBody>
     );
 }
 
@@ -62,6 +102,11 @@ const TimelineBody = styled.div`
     display: flex;
     justify-content: center;
     background-color: #333333;
+
+    @media (max-width: 614px){
+        flex-direction: column;
+        align-items: center;
+    }
 `;
 
 const TimelineContainer = styled.div`
@@ -70,6 +115,12 @@ const TimelineContainer = styled.div`
     justify-content: space-between;
     font-family: 'Lato';
     margin-top: 60px;
+
+    @media (max-width: 614px){
+        width: 100%;
+        flex-direction: column;
+        align-items: center;
+    }
 
     .no-post {
         font-size: 25px;
@@ -80,6 +131,10 @@ const TimelineContainer = styled.div`
         position: fixed;
         top: 208px;
         left: calc((100vw + 611px + 15px - 301px) / 2);
+
+        @media (max-width: 614px){
+            display: none;
+        }
     }
 `;
 
@@ -89,6 +144,12 @@ const TimelinePostsContainer = styled.ul`
     flex-direction: column;
     align-items: flex-start;
     margin-right: 25px;
+
+    @media (max-width: 614px){
+        width: 100%;
+        align-items: flex-start;
+        margin-right: 0;
+    }
 `;
 
 const Title = styled.h1`
@@ -96,4 +157,8 @@ const Title = styled.h1`
     font-size: 43px;
     color: #FFFFFF;
     margin: 60px 0 45px 0;
+
+    @media (max-width: 614px){
+        margin-left: 17px;
+    }
 `;
