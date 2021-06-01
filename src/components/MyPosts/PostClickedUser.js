e44cd9d78f13df9ea8438099a0b33907f8217a71
Iuri Magnago <iurimp91@gmail.com>
2021-06-01 14:40:31 -0300

    Acertado clique no coração nas demais páginas

--- src/components/MyPosts/PostClickedUser.js
+++ src/components/MyPosts/PostClickedUser.js
@@ -7,41 +7,39 @@ import Modal from "../Modal";
 import { FaTrash } from "react-icons/fa";
 import UserContext from "../../contexts/UserContext";
 import axios from "axios";
-import {FaPencilAlt} from 'react-icons/fa';
-import {AiFillHeart} from 'react-icons/ai';
-import ReactTooltip from 'react-tooltip';
-
+import { FaPencilAlt } from "react-icons/fa";
+import { AiFillHeart } from "react-icons/ai";
+import ReactTooltip from "react-tooltip";
 
 export default function PostClickedUser({ post, RenderPosts, RenderLikes }) {
-    const { id, text, link, linkTitle, linkDescription, linkImage, user, likes } = post;   
-    const history = useHistory();
-    const { userData } = useContext(UserContext);
-    const [modalOpen, setModalOpen] = useState(false);
-    const localUser = JSON.parse(localStorage.getItem("user"));  
-    const [control,setControl]=useState(false)
-    const [newText,setNewText]=useState(text)
-    const [disabler,setDisabler]=useState(false)
-    const inputRef=useRef();
-    let enabled=false;
+  const { id, text, link, linkTitle, linkDescription, linkImage, user, likes } =
+    post;
+  const history = useHistory();
+  const { userData } = useContext(UserContext);
+  const [modalOpen, setModalOpen] = useState(false);
+  const localUser = JSON.parse(localStorage.getItem("user"));
+  const [control, setControl] = useState(false);
+  const [newText, setNewText] = useState(text);
+  const [disabler, setDisabler] = useState(false);
+  const inputRef = useRef();
+  let enabled = false;
 
-    useEffect(()=>{
-        if(control){
-          inputRef.current.focus()
-        }
-        setNewText(text)
-    },[control]);
-
-    function ShowEdit(){ 
-        if(control){
-         setControl(false)
-         
-         return
-        }else{
-         setControl(true)
-         
-        }
-        
+  useEffect(() => {
+    if (control) {
+      inputRef.current.focus();
     }
+    setNewText(text);
+  }, [control]);
+
+  function ShowEdit() {
+    if (control) {
+      setControl(false);
+
+      return;
+    } else {
+      setControl(true);
+    }
+  }
 
   function ShowEdit() {
     if (control) {
@@ -65,94 +63,133 @@ export default function PostClickedUser({ post, RenderPosts, RenderLikes }) {
     const request = axios.put(
       `https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts/${id}`,
       body,
-      config)
+      config
+    );
 
-      request.then((response) => {
-        setDisabler(false);
-        setControl(false);
-        RenderPosts();
-      });
-      request.catch(() => {
-        alert("Não foi possível salvar as alterações");
-        setDisabler(false);
-      });
+    request.then((response) => {
+      setDisabler(false);
+      setControl(false);
+      RenderPosts();
+    });
+    request.catch(() => {
+      alert("Não foi possível salvar as alterações");
+      setDisabler(false);
+    });
+  }
+
+  function LikeOrDeslike() {
+    const body = [];
+    const config = {
+      headers: { Authorization: `Bearer ${userData.token || localUser.token}` },
+    };
+
+    if (enabled === false) {
+      const request = axios.post(
+        `https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts/${id}/like`,
+        body,
+        config
+      );
+    } else {
+      const request = axios.post(
+        `https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts/${id}/dislike`,
+        body,
+        config
+      );
     }
+    RenderLikes();
+    RenderPosts();
+  }
 
-    function LikeOrDeslike() {
-        const body = [];
-        const config = {
-          headers: { Authorization: `Bearer ${userData.token || localUser.token}` },
-        };
-    
-        if(enabled===false){
-        const request = axios.post(
-          `https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts/${id}/like`,
-          body,
-          config
-        );
-        }else{
-          const request = axios.post(
-            `https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts/${id}/dislike`,
-            body,
-            config
-          );
-          
-        }
-        RenderLikes()
-        RenderPosts()
-      }
-    
-      likes.forEach(element => {
-        if(element.userId === localUser.user.id) {
-          enabled = true;
-        }
-      });
-      
-    return(
-        <PostBox>
-            <SideMenu enabled={enabled}>
-                <Link to={`/user/${user.id}`}> 
-                    <img src={user.avatar} alt="Imagem de avatar do usuário" />
-                </Link>
-                {enabled?<AiFillHeart className="heart-icon" onClick={LikeOrDeslike}/>:<AiOutlineHeart className="heart-icon" onClick={LikeOrDeslike}/>}
-        <span data-tip={likes.length === 0 ? '' :
-          likes.length !== 1 ?
-            likes.length >= 3 ?
-              enabled ? 
-                `Você, ${likes[0]['user.username']} e outras ${likes.length-2} pessoas` : 
-                `${likes[0]['user.username']}, ${likes[1]['user.username']} e outras ${likes.length-2} pessoas` :
-            enabled ? 
-              `Você e ${localUser.user.username===likes[0]['user.username']?likes[1]['user.username']:likes[0]['user.username']} curtiram` : 
-              `${likes[0]['user.username']} e ${likes[1]['user.username']} curtiram` :
-          enabled ? 
-            `Você curtiu` :
-            `${likes[0]['user.username']} curtiu`}>
-        {likes.length} {likes.length === 1 ? "like" : "likes"}</span>
-        <ReactTooltip place="bottom" type="light" effect="float"/>
-            </SideMenu>
-            <Content>
-                <h1 onClick={() => history.push(`user/${user.id}`)}>{user.username}</h1>
-                <h2>
-                {control?          
-                [<form onSubmit={Edit}>
-                    <input type="text" required value={newText} onChange={(e) => setNewText(e.target.value)} disabled={disabler} ref={inputRef} onKeyDown={(e)=>e.keyCode==27?setControl(false):''}/>
-                </form>]           
-                :<Hashtag text={text} />}
-                </h2>
-                <Snippet href={link} target="_blank">
-                    <div className="snippet-text">
-                        <h3>{linkTitle}</h3>
-                        <h4>{linkDescription}</h4>
-                        <h5>{link}</h5>
-                    </div>
-                    <img src={linkImage} alt={linkDescription} />
-                </Snippet>
-            </Content>
-            {(userData ? userData.user.id : localUser.user.id) === user.id && <FaPencilAlt onClick={ShowEdit} className="pencil-icon"/>}
-            {(userData ? userData.user.id : localUser.user.id) === user.id && <FaTrash onClick={() => setModalOpen(true)} className="trash-icon" />}
-            <Modal RenderPosts={RenderPosts} modalOpen={modalOpen} setModalOpen={setModalOpen} postID={id} />
-        </PostBox>
-    ); 
+  likes.forEach((element) => {
+    if (element.userId === localUser.user.id) {
+      enabled = true;
+    }
+  });
+
+  return (
+    <PostBox>
+      <SideMenu enabled={enabled}>
+        <Link to={`/user/${user.id}`}>
+          <img src={user.avatar} alt="Imagem de avatar do usuário" />
+        </Link>
+        {enabled ? (
+          <AiFillHeart className="heart-icon" onClick={LikeOrDeslike} />
+        ) : (
+            <AiFillHeart stroke={"white"} strokeWidth={80} fill={"#171717"} className="heart-icon" onClick={LikeOrDeslike} />
+        )}
+        <span
+          data-tip={
+            likes.length === 0
+              ? ""
+              : likes.length !== 1
+              ? likes.length >= 3
+                ? enabled
+                  ? `Você, ${likes[0]["user.username"]} e outras ${
+                      likes.length - 2
+                    } pessoas`
+                  : `${likes[0]["user.username"]}, ${
+                      likes[1]["user.username"]
+                    } e outras ${likes.length - 2} pessoas`
+                : enabled
+                ? `Você e ${
+                    localUser.user.username === likes[0]["user.username"]
+                      ? likes[1]["user.username"]
+                      : likes[0]["user.username"]
+                  } curtiram`
+                : `${likes[0]["user.username"]} e ${likes[1]["user.username"]} curtiram`
+              : enabled
+              ? `Você curtiu`
+              : `${likes[0]["user.username"]} curtiu`
+          }
+        >
+          {likes.length} {likes.length === 1 ? "like" : "likes"}
+        </span>
+        <ReactTooltip place="bottom" type="light" effect="float" />
+      </SideMenu>
+      <Content>
+        <h1 onClick={() => history.push(`user/${user.id}`)}>{user.username}</h1>
+        <h2>
+          {control ? (
+            [
+              <form onSubmit={Edit}>
+                <input
+                  type="text"
+                  required
+                  value={newText}
+                  onChange={(e) => setNewText(e.target.value)}
+                  disabled={disabler}
+                  ref={inputRef}
+                  onKeyDown={(e) => (e.keyCode == 27 ? setControl(false) : "")}
+                />
+              </form>,
+            ]
+          ) : (
+            <Hashtag text={text} />
+          )}
+        </h2>
+        <Snippet href={link} target="_blank">
+          <div className="snippet-text">
+            <h3>{linkTitle}</h3>
+            <h4>{linkDescription}</h4>
+            <h5>{link}</h5>
+          </div>
+          <img src={linkImage} alt={linkDescription} />
+        </Snippet>
+      </Content>
+      {(userData ? userData.user.id : localUser.user.id) === user.id && (
+        <FaPencilAlt onClick={ShowEdit} className="pencil-icon" />
+      )}
+      {(userData ? userData.user.id : localUser.user.id) === user.id && (
+        <FaTrash onClick={() => setModalOpen(true)} className="trash-icon" />
+      )}
+      <Modal
+        RenderPosts={RenderPosts}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        postID={id}
+      />
+    </PostBox>
+  );
 }
 
 const PostBox = styled.li`
